d38711bd7c862558a87439f8abbaf08615a3b115
leandrop98 <leandrocpereira1998@gmail.com>
2021-07-23 20:18:41 +0200

    evaluation file

--- dataset_generation/read_dataset.py
+++ dataset_generation/read_dataset.py
@@ -4,7 +4,21 @@ import cv2 as cv
 import sys
 import math
 import numpy as np
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw 
+
 path = "../output/images"
+# Label map
+voc_labels = [x.strip() for x in open(f'obj.txt').readlines()]
+label_map = {k: v + 1 for v, k in enumerate(voc_labels)}
+rev_label_map = {v: k for k, v in label_map.items()}  # Inverse mapping
+
+# Color map for bounding boxes of detected objects from https://sashat.me/2017/01/11/list-of-20-simple-distinct-colors/
+# List of colors
+distinct_colors = ['#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080', '#ffffff', '#000000']
+
+label_color_map = {k: distinct_colors[i] for i, k in enumerate(label_map.keys())}
 
 # Read all json files
 def display_images(bbox2d,bbox3d):
@@ -13,10 +27,14 @@ def display_images(bbox2d,bbox3d):
         with open(path + "/" + json_file, 'r') as file:
             # Load json file
             data = json.load(file)
+
             # Load image
             img = cv.imread(cv.samples.findFile(path + "/" + data["image_filename"]))
+
             if img is None:
                 sys.exit("Could not read the image.")
+
+            font = ImageFont.truetype("./calibri.ttf", 15)
 
             height, width = img.shape[:2]
 
@@ -40,9 +58,24 @@ def display_images(bbox2d,bbox3d):
             if(bbox2d):
                 #Draw 2d bounding box
                 for obj in data["objects"]:
+                    cat = obj['category'].lower()
+
+                    h = label_color_map[cat.lower()].replace('#','')
+                    label_color = tuple(int(h[i:i+2], 16) for i in (0, 2, 4))
                     cv.rectangle(img,(obj['2d_bbox'][0],obj['2d_bbox'][1]),
-                    (obj['2d_bbox'][2], obj['2d_bbox'][3]),(0,0,250),2)
-                    cv.putText(img,obj['category'],(obj['2d_bbox'][0], obj['2d_bbox'][3]+20), fontFace = cv.FONT_HERSHEY_SIMPLEX, fontScale=0.5,color= (0,0,255),thickness=1)
+                    (obj['2d_bbox'][2], obj['2d_bbox'][3]),label_color,2)
+                    
+                    #cv.putText(img,obj['category'],(obj['2d_bbox'][0], obj['2d_bbox'][3]+20), fontFace = font, fontScale=0.5,color= (0,0,255),thickness=1)
+                    
+                    text_size = font.getsize(cat.upper())
+                    textbox_location_1 = (int(obj['2d_bbox'][0]), int(obj['2d_bbox'][1] - text_size[1])-5)
+                    textbox_location_2 = (int(obj['2d_bbox'][0] + text_size[0] + 10),int(obj['2d_bbox'][1]))
+                    text_location = (int(obj['2d_bbox'][0] + 2.), int(obj['2d_bbox'][1])-4)
+
+                    
+                    cv.rectangle(img,textbox_location_1,textbox_location_2, label_color,-1)
+                    cv.putText(img,obj['category'],text_location, fontFace = cv.FONT_HERSHEY_TRIPLEX                           , fontScale=0.5,color= (255,255,255),thickness=1)
+
 
             # Resize the image to add the relationships text
 
